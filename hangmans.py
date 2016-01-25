d176c743e01bd344d8c60b49e1c5cc6973cdb91d
Yurii <unid@ua.fm>
2016-01-25 23:23:20 +0200

    hangmans v - 0.4

--- hangmans.py
+++ hangmans.py
@@ -6,12 +6,21 @@ import data #импортируем из созданого файла data.py
 #импортируем с файла data.py и присвоиваим им имя для удобства
 win = data.you_win
 lose = data.you_lose
-tex = data.tex
 ABC = data.ABC
 ABC_RU = data.ABC_RU
 menu = data.menu
 
+
+
 def start(letter):  #начало игры
+    def score(sc):      #функция подсчета балов
+        if letter in data.cost_letters:
+            sc.append(data.cost_letters[letter])
+            if sum(sc) <= 25:
+                print("У Вас на счету {0} очков".format(sum(sc)))
+            else:
+                print("У Вас больше 25 очков, Вы победили")
+                sys.exit()  #если 25 очков набрали это выйдет с игры
     word = data.words()
     print("Вам нужно отгадать слово из {0} букв".format(len(word)))
     blanks = "-" * len(word)  
@@ -28,6 +37,7 @@ def start(letter):  #начало игры
                         print("Эти буквы вы уже отгадали : {0} ".format(name))
                 elif letter in word:    #если вашей буквы нет в списке
                     name.append(letter) #добавляем букву в список
+                    score(sc)
                     print(name)     #печатаем букву, которую только что добавили               
                 elif letter not in word:    #если буквы нет в загаданом слове
                     if letter in no:    #если буквы нет в списке уже вводимых слов
@@ -42,7 +52,7 @@ def start(letter):  #начало игры
                             print(lose[i])
                         if errors == 8:
                             print("Вы проиграли")
-                            print(word)
+                            #print(word)
                             break
             else:
                 print("Я не знаю такого символа")
@@ -71,13 +81,20 @@ if __name__ == '__main__':
     
     name = []       #сюда будут сохранятся буквы, слова
     no = []         #сюда будут сохранятся буквы, которые вы уже ввели
+    sc = []
+
+
 
     def input_choice():
-        choice = input("Ввод:  ")
-        return choice
+        while 2:
+            choice = input("Ввод:  ")
+            if choice == "1":    #если вы выбираете 1, игра начинается
+                return start(choice)
+            elif choice == "2":
+                data.help()
+            else:
+                print("Я ТАКОЙ КОМАНДЫ НЕ ЗНАЮ")
+        
 
     choice = input_choice()
-    if choice == "1":    #если вы выбираете 1, игра начинается
-        start(choice)
-    else:
-        print("Я ТАКОЙ КОМАНДЫ НЕ ЗНАЮ, ПОКА")
+
