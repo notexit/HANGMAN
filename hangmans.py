43a190545a8e83e4f4da892b87257cb289f34ebb
Yurii <unid@ua.fm>
2016-01-26 15:36:01 +0200

    hangmans v - 0.4

--- hangmans.py
+++ hangmans.py
@@ -6,15 +6,22 @@ import data #импортируем из созданого файла data.py
 #импортируем с файла data.py и присвоиваим им имя для удобства
 win = data.you_win
 lose = data.you_lose
-tex = data.tex
 ABC = data.ABC
 ABC_RU = data.ABC_RU
 menu = data.menu
 
 def start(letter):  #начало игры
+    def score(sc):      #функция подсчета балов
+        if letter in data.cost_letters:
+            sc.append(data.cost_letters[letter])
+            if sum(sc) <= 25:
+                print("У Вас на счету {0} очков".format(sum(sc)))
+            else:
+                print("У Вас больше 25 очков, Вы победили")
+                sys.exit()  #если 25 очков набрали это выйдет с игры
     word = data.words()
     print("Вам нужно отгадать слово из {0} букв".format(len(word)))
-    blanks = "-" * len(word)  
+    blanks = "-" * len(word)
     print(blanks)
     while blanks != word:
         blanks = "-" * len(word)
@@ -23,26 +30,27 @@ def start(letter):  #начало игры
             if letter in ABC_RU:   #если вваш ввод (в данном случае буква) есть в алфавите
                 if letter in name:  #если ваша буква есть в списке name
                     if len(name) == 1: #если в списке есть 1 буква
-                        print("Эту букву вы уже отгадали : {0} ".format(name)) 
+                        print("Эту букву вы уже отгадали : {0} ".format(name))
                     elif len(name) > 1: # если в списке больше чем 1 буква
                         print("Эти буквы вы уже отгадали : {0} ".format(name))
                 elif letter in word:    #если вашей буквы нет в списке
                     name.append(letter) #добавляем букву в список
-                    print(name)     #печатаем букву, которую только что добавили               
+                    score(sc)
+                    print(name)     #печатаем букву, которую только что добавили
                 elif letter not in word:    #если буквы нет в загаданом слове
                     if letter in no:    #если буквы нет в списке уже вводимых слов
                         if len(no) == 1:    #если длина в списке ровна 1
                             print("Вы уже предлагали такую букву : {0} ".format(no))
                         elif len(no) > 1:   # если больше чем один
-                            print("Вы уже предлагали такие буквы: {0} ".format(no))      
+                            print("Вы уже предлагали такие буквы: {0} ".format(no))
                     if letter not in no:    #если буквы нет в списке вводимых уже букв
-                        no.append(letter)   #добавляем в список вводимые уже буквы     
+                        no.append(letter)   #добавляем в список вводимые уже буквы
                         errors = len(no) + 1
                         for i in range(1,errors):
                             print(lose[i])
                         if errors == 8:
                             print("Вы проиграли")
-                            print(word)
+                            #print(word)
                             break
             else:
                 print("Я не знаю такого символа")
@@ -68,16 +76,19 @@ def start(letter):  #начало игры
 if __name__ == '__main__':
 
     menu  #я там сверху его портировал
-    
+
     name = []       #сюда будут сохранятся буквы, слова
     no = []         #сюда будут сохранятся буквы, которые вы уже ввели
+    sc = []
 
     def input_choice():
-        choice = input("Ввод:  ")
-        return choice
+        while 2:
+            choice = input("Ввод:  ")
+            if choice == "1":    #если вы выбираете 1, игра начинается
+                return start(choice)
+            elif choice == "2":
+                data.help()
+            else:
+                print("Я ТАКОЙ КОМАНДЫ НЕ ЗНАЮ")
 
     choice = input_choice()
-    if choice == "1":    #если вы выбираете 1, игра начинается
-        start(choice)
-    else:
-        print("Я ТАКОЙ КОМАНДЫ НЕ ЗНАЮ, ПОКА")
